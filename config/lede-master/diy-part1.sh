f8e7b6d20290c16ca7602919f6c68a377b90ac3f
hibeiyuus <5201507+hibeiyuus@users.noreply.github.com>
2024-08-30 17:02:45 +0800

    Update diy-part1.sh for add more feeds

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -6,8 +6,9 @@
 # Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
 #========================================================================================================================
 
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# Add feed source for fun
+sed -i '$a src-git hwfuture https://github.com/hongweifuture/openwrt-packages' feeds.conf.default
+sed -i '$a src-git pwdep https://github.com/hongweifuture/pwdep.git' feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
